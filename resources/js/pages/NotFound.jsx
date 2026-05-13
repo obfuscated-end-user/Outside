c1161f88a34e34fce84f3e26bff2e2c9fe4ab502
Yokohama <artuzcapati2@gmail.com>
2026-05-13 23:19:04 +0800

    fixed typo in css

--- resources/js/pages/NotFound.jsx
+++ resources/js/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
 	return (
 		<div className="p-8">
-			<h1 className="text-4x1 font-bold">404</h1>
+			<h1 className="text-4xl font-bold">404</h1>
 			<p className="text-gray-500">Page not found.</p><br/>
 			<p title="Touch grass, AKA go outside.">
 				Keyboard gathers dust<br/>
